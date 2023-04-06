3f109ad655a727e43d1b05eae1f977af00232b11
CSmith175 <98501848+CSmith175@users.noreply.github.com>
2023-04-06 12:19:57 -0700

    Added new scene, enemy parent and enemystats scriptableObject

---
+++ Gauntlet/Assets/Scripts/ScriptableObjects/EnemyStats.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : ScriptableObject
+{
+    public int enemyMoveSpeed;
+    public int enemyMeleeDamage;
+    public int enemyShotDamage;
+    public int enemyEngageDistance;
+    public int enemyPointValue;
+    public int enemyMaxHealth;
+}
